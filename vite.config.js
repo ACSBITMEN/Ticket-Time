7a5273996023e76542f52c38e698383351a95bd3
ACSBITMEN <dv.andressuarez@gmail.com>
2024-11-04 03:02:09 -0500

    deploy y github-page completado

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Ticket-Time/',
+  server: {
+    port: 5174, // Cambia el número de puerto al que prefieras
+    host: true, // Esto habilita la conexión en red
+  },
 })
